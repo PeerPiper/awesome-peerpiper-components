51f8ddd90f7ebc47e9daf30f1dffa3b64a06cc89
Doug A <douganderson444@gmail.com>
2022-06-11 02:40:01 -0300

    add Changable comp

---
+++ src/lib/components/@peerpiper/Changable.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+
+	// pass in the item you want to monitor changes on
+	export let item;
+
+	const makeChangable = (varObj) => ({
+		key: Object.keys(varObj)[0],
+		value: varObj[Object.keys(varObj)[0]]
+	});
+
+	let { key, value } = makeChangable(item);
+
+	// dispatch the change event when the content is edited
+	$: if (value) dispatch('change', { [key]: value });
+</script>
+
+<span contenteditable bind:textContent={value} />
