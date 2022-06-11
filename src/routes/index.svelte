b4f80a8ac8d9179ad889e0a9ef41049abbc150c2
Doug A <douganderson444@gmail.com>
2022-06-11 02:41:46 -0300

    update homepage with .svx file

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,25 @@
 <script>
 	import { Peerpiper } from '@peerpiper/awesome-peerpiper-components';
+	// import { globbySync } from 'globby';
+
+	import Changable from '$lib/components/@peerpiper/Changable.svx';
 
 	export let name = 'Douglas Anderson';
 	export let address = '247 Main Street, USA';
+
+	// iterate through each subdirectory in src/lib/components
+	// and import each component's svx file
+
+	// use globby library to get all files in src/lib/components
+	// const components = globbySync('src/lib/components/**/*.svx');
+	// console.log({ components });
 </script>
 
 <div>
 	Demo <pre>awesome-peerpiper-components</pre>
 </div>
+
+<Changable />
 
 <Peerpiper.ContactCard>
 	<span slot="name">{name}</span>
