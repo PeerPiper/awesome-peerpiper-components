2f7861047c55eb7256e59f3ccdd73fcd78d01e65
Doug A <douganderson444@gmail.com>
2022-07-13 11:50:23 -0300

    expand default readme list

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,14 +3,15 @@
 
 	import { Peerpiper } from '@peerpiper/awesome-peerpiper-components';
 
-	import ChangableReadme from '@peerpiper/awesome-peerpiper-components/components/@peerpiper/Changable.readme.svx';
 	import ContactCardReadme from '@peerpiper/awesome-peerpiper-components/components/@peerpiper/ContactCard.readme.svx';
+	import ContactListReadme from '@peerpiper/awesome-peerpiper-components/components/@peerpiper/ContactList.readme.svx';
+	import TagManagerReadme from '@peerpiper/awesome-peerpiper-components/components/@peerpiper/TagManager.readme.svx';
 </script>
 
 <main class="m-4">
-	Demo <pre>awesome-peerpiper-components</pre>
-
 	<!-- {#each Object {...Peerpiper}  Object.keys } -->
 	<!-- <ChangableReadme /> -->
 	<ContactCardReadme />
+	<ContactListReadme />
+	<TagManagerReadme />
 </main>
