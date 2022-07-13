1632628ceed640c0419894bb3fc13867d3ed792d
Doug A <douganderson444@gmail.com>
2022-07-13 11:50:51 -0300

    ComponentMounterReadme

---
+++ src/routes/mounter.svelte
@@ -0,0 +1,13 @@
+<script>
+	// @ts-nocheck
+
+	import ComponentMounterReadme from '@peerpiper/awesome-peerpiper-components/components/@peerpiper/ComponentMounter.readme.svx';
+</script>
+
+<main class="m-4">
+	Demo <pre>awesome-peerpiper-components</pre>
+
+	<!-- {#each Object {...Peerpiper}  Object.keys } -->
+	<!-- <ChangableReadme /> -->
+	<ComponentMounterReadme />
+</main>
